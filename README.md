88173691ad2d05dda4c4fe094ac8a6e121a5b766
Kelvin Ha <haleduykhang@gmail.com>
2024-04-08 19:35:50 +0700

    add src dir

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # golang-test-coverage
+
+## backend coverage
 ![Coverage](https://img.shields.io/badge/Coverage-100.0%25-brightgreen)
---
+++ src/add_test.go
@@ -0,0 +1,33 @@
+package src
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "1 + 1 = 2",
+			args: args{a: 1, b: 1},
+			want: 2,
+		},
+		{
+			name: "1 + 3 = 4",
+			args: args{a: 1, b: 3},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
